afe577d699833a2e0ec9064f88469611a5d73e39
jonatas <jonatas_dev@hotmail.com>
2024-11-10 14:05:47 -0300

    AT

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import intites.Product;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        Product product = new Product();
+        System.out.println("Insira os dados do produto: ");
+        System.out.print("name: ");
+        product.name = sc.nextLine();
+        System.out.print("Price: ");
+        product.price = sc.nextDouble();
+        System.out.print("quantidade em estoque: ");
+        product.quantity= sc.nextInt();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.print(product.name + "," + product.price + "," + product.quantity);
+
+        sc.close();
     }
 }
--- src/intites/Product.java
+++ src/intites/Product.java
@@ -1,4 +1,23 @@
 package intites;
-
+//Essa classe tem por objetivo executar uma operação de estoque.
+//Realizando entrada e saida de produtos e sua quantidade.
 public class Product {
+    //Atributos da classe.
+    public String name;
+    public double price;
+    public int quantity;
+
+    //Funções da classe.
+    public double totalValueInStock(){
+        return price * quantity;
+    }
+    public void addProducts (int quantity){
+        this.quantity += quantity;
+        /*A palavra reservada this é usada para se referir ao atributo da minha classe.
+         * o quantity qeu fou declarado no inicio.*/
+    }
+    public void removeProducts (int quantity){
+
+        this.quantity -= quantity;
+    }
 }
